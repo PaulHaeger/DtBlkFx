42801e4922f96e6d0e848d01888b98e7242dad90
Paul Häger <paul@paulhaeger.xyz>
2021-05-08 15:08:56 +0200

    Added first batch of files, some annotation and some work in the Processor

--- src/DtBlkFxProcessor.cpp
+++ src/DtBlkFxProcessor.cpp
@@ -139,31 +139,14 @@ void DtBlkFxAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
                                          juce::MidiBuffer& midiMessages)
 {
   juce::ignoreUnused(midiMessages);
+  //SCOPE_NO_FP_EXCEPTIONS_OR_DENORMALS;
 
-  juce::ScopedNoDenormals noDenormals;
-  auto totalNumInputChannels = getTotalNumInputChannels();
-  auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-  // In case we have more outputs than inputs, this code clears any output
-  // channels that didn't contain input data, (because these aren't
-  // guaranteed to be empty - they may contain garbage).
-  // This is here to avoid people getting screaming feedback
-  // when they first compile a plugin, but obviously you don't need to keep
-  // this code if your algorithm always overwrites all the output channels.
-  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-    buffer.clear(i, 0, buffer.getNumSamples());
-
-  // This is the place where you'd normally do the guts of your plugin's
-  // audio processing...
-  // Make sure to reset the state if your inner loop is processing
-  // the samples and the outer loop is handling the channels.
-  // Alternatively, you can process the samples with the channels
-  // interleaved by keeping the same state.
-  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-    auto* channelData = buffer.getWritePointer(channel);
-    juce::ignoreUnused(channelData);
-    // ..do something to the data...
+  _process(inputs, samps);
+  for (int i = 0; i < buffer.getNumChannels(); ++i) {
+    PAddOut p(outputs[i]);
+    wrapProcess(p, _chan[i].x3, _x3_o, samps);
   }
+  _x3_o = wrap(samps + _x3_o, _chan[0].x3);
 }
 
 bool DtBlkFxAudioProcessor::hasEditor() const
--- src/DtBlkFxProcessor.h
+++ src/DtBlkFxProcessor.h
@@ -17,6 +17,8 @@
  * along with DtBlkFx.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "MorphParam.h"
+
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
@@ -55,4 +57,42 @@ public:
 
 private:
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DtBlkFxAudioProcessor)
+public:
+  MorphParam _mixback_param;
+  MorphParam _delay_param;
+  MorphParam _fft_len_param;
+  MorphParam _overlap_param;
+
+  MorphParam _pwr_match_param; // 0=filter mode, 1=pwr match
+  MorphParam _beat_sync_param;
+  MorphParam _param_sync_param;
+
+  // blk fraction to be used as a shoulder - the shoulder is extra data on
+  // the left and right of the blk that is transformed but not mixed back to output
+  MorphParam _blk_shoulder_frac_param;
+
+  // window applied to shoulder (only applicable when _blk_shoulder_frac_param is > 0)
+  MorphParam _blk_shoulder_wdw_param;
+
+  // interpolation envelope for blk mix
+  MorphParam _blk_mix_param;
+
+  // which vst param index is in focus in the GUI
+  VstParamIdx _vst_param_idx_focus;
+  void* _vst_param_idx_focus_cookie;
+
+  // access program number
+  int currProgramNum() { return limit_range(curProgram, 0, (int)_program.size() - 1); }
+
+  // access current preset
+  BlkFxProgram& currProgram() { return _program[currProgramNum()]; }
+
+public: // gui state stuff
+  //
+  bool _param_morph_mode;
+  bool isMorphMode() const { return _param_morph_mode; }
+
+public:
+  // contiguous space for chunk data & presets when saving
+  std::valarray<unsigned char> _chunk_data;
 };
